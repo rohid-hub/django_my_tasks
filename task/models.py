b84b7d96772e4eac6ba97f8464a8ac0d7707025b
Rohidul Islam <rohidwork303@gmail.com>
2020-02-28 22:08:53 +0600

    List item card color changed

--- task/models.py
+++ task/models.py
@@ -4,6 +4,7 @@ class Task(models.Model):
   content = models.CharField(max_length=500)
   isImportent = models.BooleanField(default=False, null=True)
   isCompleted = models.BooleanField(default=False, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.content
