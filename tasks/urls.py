d7728c4c724a7d47f63f221a645234c2cbe7b54c
Rohidul Islam <rohidwork303@gmail.com>
2020-02-28 20:48:23 +0600

    separeted completed section, icon insted of text and collapse

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, tasks, delete, isCompleted, isImportent
+from .views import home, tasks, delete, isCompleted, isImportent, dltCompletedTasks
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('delete/<task_id>', delete, name="delete"),
     path('isCompleted/<task_id>', isCompleted, name="isCompleted"),
     path('isImportent/<task_id>', isImportent, name="isImportent"),
+    path('tasks/dltCompletedTasks', dltCompletedTasks),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from task.models import Task
 from task.forms import AddTaskForm
-def home(request):
-  return render(request, 'home.html')
+
+def home(request):  
+  all_Tasks = Task.objects.filter(isCompleted=False)
+  return render(request, 'home.html', {'tasks': all_Tasks})
 
 
 def tasks(request):
@@ -11,18 +13,19 @@ def tasks(request):
     form = AddTaskForm(request.POST or None)
     if form.is_valid():
       form.save()
-      messages.success(request, "Task item added to tasks!")
+      messages.success(request, "Task item has been added to Tasks list!")
       return redirect('tasks')
     else:
       return redirect('tasks')
   else:
-    tasks_items = Task.objects.all()
-    return render(request, 'tasks/tasks.html', {'tasks': tasks_items})
+    all_Tasks = Task.objects.filter(isCompleted=False)
+    completed_Tasks = Task.objects.filter(isCompleted=True)
+    return render(request, 'tasks/tasks.html', {'tasks': all_Tasks, 'completed_Tasks': completed_Tasks})
 
 def delete(request, task_id):
   task_item_to_dlt = Task.objects.get(pk=task_id)
   task_item_to_dlt.delete()
-  messages.warning(request, "Task deleted!")
+  messages.warning(request, "Task item has been deleted!")
   return redirect('tasks')
 
 
@@ -44,4 +47,11 @@ def isImportent(request, task_id):
   else:
     item.isImportent = False
     item.save()
+  return redirect('tasks')
+
+def dltCompletedTasks(request):
+  tasks = Task.objects.filter(isCompleted=True)
+  for task in tasks:
+    task.delete()
+  messages.success(request, 'All completed tasks has been deleted!')
   return redirect('tasks')
